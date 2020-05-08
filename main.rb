8391090392db2cc7f4d26d7b103a25a06482148c
november31 <51793092+november31@users.noreply.github.com>
2020-05-08 23:35:02 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ Window.load_resources do
   GROUND_Y = 400
 
   Window.loop do
-    Window.draw_font(0, 0, "Hello!!!!!", Font.default, color: C_WHITE)
+    Window.draw_font(0, 0, "Hello!!!!!星", Font.default, color: C_WHITE)
     # 背景を描画
     Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
     Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
