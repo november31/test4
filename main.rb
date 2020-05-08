64c131cc45860c6ba248b1dfe5fe8c7b8bbb3621
november31 <51793092+november31@users.noreply.github.com>
2020-05-08 13:58:26 +0900

    Add files via upload

--- main.rb
+++ main.rb
@@ -3,7 +3,14 @@ include DXOpal
 Window.load_resources do
   Window.bgcolor = C_BLACK
 
+  # 地面のY座標
+  GROUND_Y = 400
+
   Window.loop do
     Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    # 背景を描画
+    Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
+    Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
+
   end
 end
