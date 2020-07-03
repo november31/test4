d6604b4d1fee6a61907bde57544699a7fcc34996
november31 <51793092+november31@users.noreply.github.com>
2020-07-04 02:00:10 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require "dxopal"
 include DXOpal
 
-# 地面のY座標
+# 地面のY座標は400とする
 GROUND_Y = 400
+#使いたい画像を宣言する
+Image.register(:player, 'images/playre.png')
 
 Window.load_resources do
          Window.loop do
@@ -10,5 +12,7 @@ Window.load_resources do
                   Window.draw_box_fill(0,0,Window.width, GROUND_Y,[128,255,255])
                   Window.draw_box_fill(0,GROUND_Y,Window.width,Window.height,[0,128,0])
          
+                  #プレイヤーキャラを描画
+                  Window.draw(240, GROUND_Y - Image[:player].height, Image[:player])
          end
 end
