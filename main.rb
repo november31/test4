fb37d4a0629be184c1e3f1a21c7993c3402093c2
november31 <51793092+november31@users.noreply.github.com>
2020-07-04 01:53:21 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,14 @@
 require "dxopal"
 include DXOpal
 
+# 地面のY座標
+GROUND_Y = 400
+
 Window.load_resources do
          Window.loop do
+#背景を描画
+                  Window.draw_box_fill(0.0.Window.width, GROUND_Y,[128,255,255])
+                  Window.draw_box_fill(0,GROUND_Y,Window.width,Window.height,[0,128,0])
+         
          end
 end
