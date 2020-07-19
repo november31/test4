6f99431df64e484a18675e1a8248f899494b558f
november31 <51793092+november31@users.noreply.github.com>
2020-07-20 05:39:53 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -7,12 +7,16 @@ GROUND_Y = 400
 Image.register(:player, 'images/player.png')
 
 Window.load_resources do
-         #変数の初期化
-         x = 0
+         #最初は真ん中にする
+         x = Window.width / 2
          
          Window.loop do
-                  #毎フレーム8ピクセルずつ進む
+                  #キー入力をチェック
+                  if input.key_down?(K_LEFT)
+                           x -=8
+                  elsif input.key_down?(K_RIGHT)
                   x += 8
+                  end
 #背景を描画
                   Window.draw_box_fill(0,0,Window.width, GROUND_Y,[128,255,255])
                   Window.draw_box_fill(0,GROUND_Y,Window.width,Window.height,[0,128,0])
