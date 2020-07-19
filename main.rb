8504b43b56a7cd8ab3121645df75962f14167e06
november31 <51793092+november31@users.noreply.github.com>
2020-07-20 05:32:53 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -7,12 +7,17 @@ GROUND_Y = 400
 Image.register(:player, 'images/player.png')
 
 Window.load_resources do
+         #変数の初期化
+         x = 0
+         
          Window.loop do
+                  #毎フレーム8ピクセルずつ進む
+                  x += 8
 #背景を描画
                   Window.draw_box_fill(0,0,Window.width, GROUND_Y,[128,255,255])
                   Window.draw_box_fill(0,GROUND_Y,Window.width,Window.height,[0,128,0])
          
                   #プレイヤーキャラを描画
-                  Window.draw(240, GROUND_Y - Image[:player].height, Image[:player])
+                  Window.draw(x, GROUND_Y - Image[:player].height, Image[:player])
          end
 end
