9a0add61b83951d481b4e99736d9a5c80e684484
november31 <51793092+november31@users.noreply.github.com>
2020-08-19 11:16:46 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -29,6 +29,9 @@ Window.load_resources do
                         end
                                  
                   end
+                  #キャッシュ消去できてるかの確認のための文字
+                  Window.draw_font(100, 100, Ver1, Font.default)
+                  
 #背景を描画
                   Window.draw_box_fill(0,0,Window.width, GROUND_Y,[128,255,255])
                   Window.draw_box_fill(0,GROUND_Y,Window.width,Window.height,[0,128,0])
