49449a1d9ffb703a3291ef3e3279d6dba3082f91
november31 <51793092+november31@users.noreply.github.com>
2020-08-19 11:07:38 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -10,6 +10,9 @@ Window.load_resources do
          #最初は真ん中にする
          x = Window.width / 2
          
+         #タッチ位置の計算
+         touchposi = 0
+         
          Window.loop do
                   #キー入力をチェック
                   if Input.key_down?(K_LEFT)
@@ -18,7 +21,12 @@ Window.load_resources do
                   x += 8
                            
                   elsif Input.touch_down?
+                           
+                        if self.touch_x > Window.width / 2
                            x += 8
+                        elsif self.touch_x < Window.width / 2
+                                 x -= 8
+                                 
                   end
 #背景を描画
                   Window.draw_box_fill(0,0,Window.width, GROUND_Y,[128,255,255])
